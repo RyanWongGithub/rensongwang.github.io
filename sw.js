17d7619267d3f47592ef372c26e441ce454ea951
rensongwang <1018440763@qq.com>
2019-04-19 10:58:01 +0800

    Site updated: 2019-04-19 10:57:59

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","88f4e84b57b1ede9d8e51d1c906fffc5"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
